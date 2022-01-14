ba67f098d85fbd9241023c2caa148c2dd1976e20
Isaias Gonzalez <isaias.gonzalez@mayahtt.com>
2022-01-14 16:46:59 -0500

    useContext

--- src/component/Four.tsx
+++ src/component/Four.tsx
@@ -1,16 +1,8 @@
-import React, { useContext, useEffect, useRef, useState } from 'react';
-import { createTrackedSelector, memo } from 'react-tracked';
+import React, { useContext, useEffect, useRef } from 'react';
 import AppContext from '../store/app-context';
-import { useStore } from '../store/state';
 
-const useTrackedStore = createTrackedSelector(useStore);
-
-export const Four = memo(() => {
-    const four = useStore(state => state.countFour);
-    const increaseFour = useStore(state => state.increaseFour);
-    const trackedState = useTrackedStore();
+export const Four = (() => {
     const appContext = useContext(AppContext);
-    const [count, setCount] = useState(0);
     const renders = useRef(1);
 
     useEffect(() => {renders.current++});
@@ -18,10 +10,7 @@ export const Four = memo(() => {
     return (
         <>
             Four Renders: {renders.current}
-            <button onClick={() => setCount(c => c + 1)}>useState {count}</button>
             <button onClick={() => appContext.changeState({ ...appContext, countFour: appContext.countFour+1 })}>useContext {appContext.countFour}</button>
-            <button onClick={() => increaseFour()}>Zustand {four}</button>
-            <button onClick={() => trackedState.increaseFour()}>Zustand tracked {trackedState.countFour}</button>
         </>
     );
 });
--- src/component/Two.tsx
+++ src/component/Two.tsx
@@ -1,31 +1,17 @@
-import React, { useContext, useEffect, useRef, useState } from 'react';
-import { createTrackedSelector, memo } from 'react-tracked';
+import React, { useContext, useEffect, useRef } from 'react';
 import AppContext from '../store/app-context';
-import { useStore } from '../store/state';
 import { Three } from './Three';
 
-const useTrackedStore = createTrackedSelector(useStore);
-
-export const Two = memo(() => {
-    const two = useStore(state => state.countTwo);
-    const increaseTwo = useStore(state => state.increaseTwo);
+export const Two = (() => {
     const appContext = useContext(AppContext);
-    const [count, setCount] = useState(0);
-    const trackedState = useTrackedStore();
-
     const renders = useRef(1);
-
 
     useEffect(() => { renders.current++ });
 
     return (
         <>
             Two Renders: {renders.current}
-            <button onClick={() => setCount(c => c + 1)}>useState {count}</button>
             <button onClick={() => appContext.changeState({ ...appContext, countTwo: appContext.countTwo + 1 })}>useContext {appContext.countTwo}</button>
-            <button onClick={() => increaseTwo()}>Zustand {two}</button>
-            <button onClick={() => trackedState.increaseTwo()}>Zustand tracked {trackedState.countTwo}</button>
-
             <div>
                 <Three />
             </div>
